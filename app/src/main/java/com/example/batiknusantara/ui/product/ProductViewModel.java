eb54efea911c8f7f22fac6a4026b8d820eca1c94
CyDreal <ditodiaz25@gmail.com>
2025-06-18 15:16:42 +0700

    update: bottom navigation

--- app/src/main/java/com/example/batiknusantara/ui/product/ProductViewModel.java
+++ app/src/main/java/com/example/batiknusantara/ui/product/ProductViewModel.java
@@ -1,16 +1,16 @@
-package com.example.batiknusantara.ui.notifications;
+package com.example.batiknusantara.ui.product;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class ProductViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public ProductViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is product fragment");
     }
 
     public LiveData<String> getText() {
